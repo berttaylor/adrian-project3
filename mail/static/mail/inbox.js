7114525aac93c012c44e350319aad9c44d5c863e
Adrian <adrian.register@yahoo.co.uk>
2023-10-18 13:28:22 +0200

    slow 'progress'

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,3 +1,45 @@
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Show inbox elements
+  if (mailbox === 'sent') {
+    document.querySelector('#para').innerHTML = 'IN PROGRESS';    
+    fetch('/emails/sent')
+    .then(response => response.json())
+    .then(emails => {
+    console.log(emails);
+    for (const email of emails) {
+      document.querySelector('#email-sender').innerHTML = `<strong>${email.sender}</strong>`;
+      document.querySelector('#email-subject').innerHTML = `${email.subject}`;
+      document.querySelector('#email-timestamp').innerHTML = `${email.timestamp}`;
+      console.log('next email follows:');
+  }
+  }); 
+  } else {
+    document.querySelector('#para').innerHTML = ''; 
+  }
+
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
@@ -9,25 +51,3 @@ document.addEventListener('DOMContentLoaded', function() {
   // By default, load the inbox
   load_mailbox('inbox');
 });
-
-function compose_email() {
-
-  // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
-
-  // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
-}
-
-function load_mailbox(mailbox) {
-  
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
-
-  // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-}
